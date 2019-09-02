3b57681e8fd7301da0bc9417c95666c8e5378aec
Jason Taylor <jasongt@outlook.com>
2019-09-03 09:30:21 +1000

    Initial commit

---
+++ Data/TestDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecureSpa.Data
+{
+    public class TestDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Membership> Memberships { get; set; }
+    }
+}
