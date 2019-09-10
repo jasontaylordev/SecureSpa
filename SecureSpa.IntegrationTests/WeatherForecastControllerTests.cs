c531aecb85a3a0a55d4eb7607927ee06a805eb11
Jason Taylor <jasongt@outlook.com>
2019-09-10 16:27:32 +1000

    First attempt.

--- SecureSpa.IntegrationTests/WeatherForecastControllerTests.cs
+++ SecureSpa.IntegrationTests/WeatherForecastControllerTests.cs
@@ -1,11 +1,16 @@
+using System.Collections.Generic;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 
 namespace SecureSpa.IntegrationTests
 {
-    public class WeatherForecastControllerTests
-        : IClassFixture<WebApplicationFactory<Startup>>
+    public class WeatherForecastControllerTests : IClassFixture<WebApplicationFactory<Startup>>
     {
         private readonly WebApplicationFactory<Startup> _factory;
 
@@ -18,13 +23,34 @@ namespace SecureSpa.IntegrationTests
         public async Task Get_WhenAuthenticated_ReturnsSuccessResult()
         {
             // Arrange
-            var client = _factory.CreateClient();
+            var client = _factory.WithWebHostBuilder(builder => builder.ConfigureTestServices(
+                services => services.AddMvc(
+                    options =>
+                    {
+                        options.Filters.Add(new AllowAnonymousFilter());
+                        options.Filters.Add(new FakeUserFilter());
+                    }))).CreateClient();
 
             // Act
             var response = await client.GetAsync("/weatherforecast/");
 
             // Assert
             response.EnsureSuccessStatusCode();
+        }
+
+        private class FakeUserFilter : IAsyncActionFilter
+        {
+            public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+            {
+                context.HttpContext.User = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+                {
+                    new Claim(ClaimTypes.NameIdentifier, "12345678-1234-1234-1234-123456789012"),
+                    new Claim(ClaimTypes.Name, "TestUser"),
+                    new Claim(ClaimTypes.Email, "test.user@example.com"), // add as many claims as you need
+                }));
+
+                await next();
+            }
         }
     }
 }
