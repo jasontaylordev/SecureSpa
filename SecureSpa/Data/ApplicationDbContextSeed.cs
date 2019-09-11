a2113ea32a2ddd65673e1ff027068aaaed9cf99e
Jason Taylor <jasongt@outlook.com>
2019-09-11 11:02:29 +1000

    Add demo user seed.

---
+++ SecureSpa/Data/ApplicationDbContextSeed.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using SecureSpa.Models;
+
+namespace SecureSpa.Data
+{
+    public class ApplicationDbContextSeed
+    {
+        public static async Task SeedAsync(UserManager<ApplicationUser> userManager)
+        {
+            var defaultUser = new ApplicationUser { UserName = "demouser@northwind", Email = "demouser@northwind" };
+            await userManager.CreateAsync(defaultUser, "Pass@word1");
+        }
+    }
+}
--- SecureSpa/Program.cs
+++ SecureSpa/Program.cs
@@ -1,19 +1,39 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using SecureSpa.Data;
+using SecureSpa.Models;
 
 namespace SecureSpa
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+                try
+                {
+                    var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+                    
+                    await ApplicationDbContextSeed.SeedAsync(userManager);
+                }
+                catch (Exception ex)
+                {
+                    var logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
